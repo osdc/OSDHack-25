85cfd47b29098e27eba8d2d8e9e59c2d46a5a8a7
akshit <narbhakshit@gmail.com>
2025-04-14 22:54:34 +0530

    explictly added output dr in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  build: {
+    outDir: 'dist', // Vercel expects output folder
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

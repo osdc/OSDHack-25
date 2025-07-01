c9a5e5cf4b3a410ebb44720da4b603545252efc7
Ritika Jain <jainritikaa05@gmail.com>
2025-07-01 22:32:19 +0530

    Rulebook Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -150,7 +150,7 @@ export default function Home() {
       text: "RULEBOOK",
       tint: "tint-[#45b7d1]",
       modalHeading: "RULEBOOK",
-      modalContent: "TBA",
+      modalContent: <Stuff/>,
     },
     {
       src: "hourglass.png",
